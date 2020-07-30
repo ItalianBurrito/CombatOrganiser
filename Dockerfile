805e4d7d1778bb1d60ad131904f51192a0e5c0e5
ItalianBurrito <eat_lead76@hotmail.com>
2020-07-30 15:22:50 -0700

    Tweaked tables, added php code to connect and test Players Table and database

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM php:7.3.0-apache
 
 RUN a2enmod rewrite
-RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli && docker-php-ext-install pdo pdo_mysql
 
 ADD ./DungeonMaster/ /var/www/html
 
--- DungeonMaster/Client/Combat_DM_View/index.php
+++ DungeonMaster/Client/Combat_DM_View/index.php
@@ -1,1 +1,44 @@
+<?php
+$servername = "172.17.0.3";
+$username = "root";
+$password = "DNDisforCoolDOODES42069";
 
+try{
+  $conn = new PDO("mysql:host=$servername; dbname=Dungeons",$username, $password);
+  //Set the PDO error mode to exception.
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+  echo "<br>";
+
+  $stmt = $conn->prepare("INSERT INTO PLAYERS (PLAYER_NAME, PLAYER_PASS, PLAYER_EMAIL)
+  VALUES (:name,:pass ,:email )");
+  $stmt->bindParam(':name', $name);
+  $stmt->bindParam(':pass', $pass);
+  $stmt->bindParam(':email', $email);
+
+  $name = "Eric Puls";
+  $pass = password_hash("DungeonsAndDragons", PASSWORD_DEFAULT);
+  $email = "eat_lead76@hotmail.com";
+  $stmt->execute();
+
+  echo "New record created successfully";
+  echo "<br>";
+
+  $stmt = $conn->prepare("SELECT * FROM PLAYERS");
+  $stmt->execute();
+
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+  foreach($stmt->fetchAll() as $col=>$row){
+    foreach($row as $lineitem){
+      echo $lineitem;
+      echo "<br>";
+    }
+  }
+
+}catch(PDOException $e){
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$conn = null;
+?>
