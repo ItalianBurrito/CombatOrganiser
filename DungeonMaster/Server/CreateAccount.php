c5f32272b7b6078998d8595106559e10442be71a
ItalianBurrito <eat_lead76@hotmail.com>
2020-08-13 16:03:51 -0700

    Finished form validation for 'Create Account'.

--- DungeonMaster/Server/CreateAccount.php
+++ DungeonMaster/Server/CreateAccount.php
@@ -3,7 +3,7 @@ session_start();
 if(!empty($_SESSION['login'])){
   header("location: CampaignList.php");
 }
-//include 'DB_Connect.php';//Change to DB_Query that has nothing but search priv
+//require 'DB_Connect.php';//Change to DB_Query that has nothing but search priv
 
 $nameErr = $emailErr = $passwordErr = $confirmPasswordErr = '';
 $name = $email = $password = $confirmPassword = '';
@@ -11,11 +11,16 @@ $name = $email = $password = $confirmPassword = '';
 //Check is the submit button has been pressed
 if($_SERVER["REQUEST_METHOD"] == "POST"){
   if(empty($_POST['name'])){$nameErr = 'Name is required.';}
-  else{$name = scrub_input($_POST['name']);}
+  else{$name = scrub_input($_POST['name']);
+    if(!preg_match("/^[a-zA-Z ]*$/",$name)){$nameErr = 'Only letters and white space allowed.';}}
+
   if(empty($_POST['email'])){$emailErr = 'Email is required.';}
-  else{$email = scrub_input($_POST['email']);}
+  else{$email = scrub_input($_POST['email']);
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){$emailErr = 'Invalid email format.';}}
+
   if(empty($_POST['password'])){$passwordErr = 'Password is required.';}
   else{$password = scrub_input($_POST['password']);}
+
   if(empty($_POST['confirmPassword'])){$confirmPasswordErr = 'Must confirm password.';}
   else{$confirmPassword = scrub_input($_POST['confirmPassword']);}
 }
@@ -34,15 +39,15 @@ function scrub_input($data){
 <body>
   <form method='post' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
     <p>Name</p>
-    <input type='text' name='name'>
+    <input type='text' name='name' value='<?php echo $name;?>'>
     <span class='error'>* <?php echo $nameErr;?></span>
 
     <p>Email</p>
-    <input type='text' name='email'>
+    <input type='text' name='email' value='<?php echo $email;?>'>
     <span class='error'>* <?php echo $emailErr;?></span>
 
     <p>Password</p>
-    <input type='password' name='password'>
+    <input type='password' name='password' value='<?php echo $password;?>'>
     <span class='error'>* <?php echo $passwordErr;?></span>
 
     <p>Confirm Password</p>
